94dce4ff9cfc362e005fd48a44a699cd5cb74fdb
Pedro-En <characayo.pedro@ficct.uagrm.edu.bo>
2023-06-26 12:07:44 -0400

    Add title

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8">
     <meta content="width=device-width, initial-scale=1.0" name="viewport">
 
-    <title>C4SOFTWARE</title>
+    <title>UML SOFTWARE</title>
     <meta content="" name="descriptison">
     <meta content="" name="keywords">
 
